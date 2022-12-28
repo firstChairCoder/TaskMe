e3d684a8a319792eb3ee905d6948dc5aa4c78a9a
firstChairCoder <md1qq7@gmail.com>
2022-12-28 15:52:40 +0100

    TEST

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,24 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      [
+        "module-resolver",
+        {
+          alias: {
+            "db/*": ["src/db/*"],
+      "theme/*": ["src/theme/*"],
+      "components/*": ["src/components/*"],
+      "constants/*": ["src/constants/*"],
+      "i18n/*": ["src/i18n/*"],
+      "hooks/*": ["src/hooks/*"],
+      "navigation/*": ["src/navigation/*"],
+      "screens/*": ["src/screens/*"]
+          },
+        },
+      ],
+      "react-native-reanimated/plugin"
+    ],
   };
 };
