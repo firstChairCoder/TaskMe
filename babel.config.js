108e491a58408f9e99494cd7df13ce42788a8430
firstChairCoder <md1qq7@gmail.com>
2022-12-29 13:14:08 +0100

    fix: fix absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,17 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
       [
         "module-resolver",
         {
-          alias: {
-            "db/*": ["src/db/*"],
-      "theme/*": ["src/theme/*"],
-      "components/*": ["src/components/*"],
-      "constants/*": ["src/constants/*"],
-      "i18n/*": ["src/i18n/*"],
-      "hooks/*": ["src/hooks/*"],
-      "navigation/*": ["src/navigation/*"],
-      "screens/*": ["src/screens/*"]
-          },
-        },
+          root: ["."],
+          alias: { "@": "./src" }
+        }
       ],
       "react-native-reanimated/plugin"
-    ],
+    ]
   };
 };
